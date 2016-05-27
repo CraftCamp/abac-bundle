76f5dbbce682fabdc60aa8fb39ce5a82e6efba3e
Axel Venet <kern046@gmail.com>
2016-05-27 17:18:41 +0200

    Remove unused statements

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,11 +26,6 @@ class Configuration implements ConfigurationInterface
                 ->end()
             ->end()
         ;
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
         return $treeBuilder;
     }
 }
