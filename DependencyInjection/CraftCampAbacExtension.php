8fa7abb257e699bf42d06b6e92cf15b03e5845ed
Axel Venet <kern046@gmail.com>
2018-10-14 13:35:33 +0200

    Implement configuration for ABAC dependency injection

--- DependencyInjection/CraftCampAbacExtension.php
+++ DependencyInjection/CraftCampAbacExtension.php
@@ -5,7 +5,8 @@ namespace CraftCamp\AbacBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -19,11 +20,12 @@ class CraftCampAbacExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+        $loader->load('services.yaml');
+        
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         
-        $container->setDefinition('craftcamp_abac.security', (new Definition('PhpAbac\Abac', [
-            $config['configuration_files']
-        ]))->setPublic(true));
+        $container->setParameter('craftcamp_abac.configuration_files', $config['configuration_files']);
     }
 }
