260a5ebd3977c9a7e9e999bc4eab68f13543eded
Axel Venet <kern046@gmail.com>
2018-10-14 18:01:39 +0200

    Implement tests for dependency injection configuration

--- DependencyInjection/CraftCampAbacExtension.php
+++ DependencyInjection/CraftCampAbacExtension.php
@@ -27,7 +27,7 @@ class CraftCampAbacExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
         
         $container->setParameter('craftcamp_abac.configuration_files', $config['configuration_files']);
-        $container->setParameter('craftcamp_abac.cache_options', $config['cache_options']);
-        $container->setParameter('craftcamp_abac.attribute_options', $config['attribute_options']);
+        $container->setParameter('craftcamp_abac.cache_options', (isset($config['cache_options'])) ? $config['cache_options'] : []);
+        $container->setParameter('craftcamp_abac.attribute_options', (isset($config['attribute_options'])) ? $config['attribute_options'] : []);
     }
 }
